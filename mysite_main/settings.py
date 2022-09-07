5125b63284e3c2727de4ec49c93eb1059b444ceb
Sandip Sonawane <sandips.nemesis@gmail.com>
2022-09-06 23:24:14 -0500

    finalized deployment version

--- mysite_main/settings.py
+++ mysite_main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_6xhppo#^5m^8&purmb1olq@$bt*t$=&sbj7c%a9z#hvvp9rj&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
